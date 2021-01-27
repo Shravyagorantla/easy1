4428a42fccedf683f2ceff9c9cf20382a1696359
Shravyagorantla <78067051+Shravyagorantla@users.noreply.github.com>
2021-01-27 23:28:10 +0530

    Add files via upload

--- casestudy1/Program.cs
+++ casestudy1/Program.cs
@@ -4,33 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace casestudy1
+namespace Medium_q1
 {
-	class EmployeePromotion
-	{
-		static void Main(string[] args)
-		{
-			List<string> employeenames = new List<string>();
-			for (int i = 1; i <= 5; i++)
-
-			{
-				employeenames.Add(Console.ReadLine());
-			}
-			Console.WriteLine("employee names in a list");
-			foreach(var list in employeenames)
-			{
-				Console.WriteLine(list + "");
-		    }
-
-			Console.WriteLine("Please enter the name ");
-			string name= Console.ReadLine();
-			int index = employeenames.IndexOf(name);
-
-			Console.WriteLine(index+"");
-			Console.ReadLine();
+    class Program
+    {
+        public static void DisplayMenu()
+        {
+            Data.Line();
+            Console.WriteLine("Please enter the option\n1.Add an employee\n2.Modify an employee detail\n3.Print all employee's details\n4.Print an employee detail\n5.Exit");
+            Data.Line();
+        }
 
 
+        static void Main(string[] args)
+        {
 
-		}
-	}
+            for (int i = 0; ; i++)
+            {
+                DisplayMenu();
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Data.AddEmployee();
+                        break;
+                    case 2:
+                        Data.EditEmployee();
+                        break;
+                    case 3:
+                        Data.PrintAllEmployees();
+                        break;
+                    case 4:
+                        Data.PrintAndDeleteEmployee();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("invalid input \nSelect again ");
+                        break;
+
+                }
+
+
+
+            }
+
+
+        }
+
+
+    }
 }
+

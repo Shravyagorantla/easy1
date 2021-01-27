98197252f1a95e43154e7cc6a938503fd7568afa
shravya <shravya.gorantla@kanini.com>
2021-01-27 23:22:28 +0530

    Add project files.

---
+++ casestudy1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casestudy1
+{
+	class EmployeePromotion
+	{
+		static void Main(string[] args)
+		{
+			List<string> employeenames = new List<string>();
+			for (int i = 1; i <= 5; i++)
+
+			{
+				employeenames.Add(Console.ReadLine());
+			}
+			Console.WriteLine("employee names in a list");
+			foreach(var list in employeenames)
+			{
+				Console.WriteLine(list + "");
+		    }
+
+			Console.WriteLine("Please enter the name ");
+			string name= Console.ReadLine();
+			int index = employeenames.IndexOf(name);
+
+			Console.WriteLine(index+"");
+			Console.ReadLine();
+
+
+
+		}
+	}
+}
